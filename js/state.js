ade7db1d4aa45a3ef007627664d3d774229c90c0
saif782005 <111455105+saif782005@users.noreply.github.com>
2026-04-28 02:31:04 +0300

    Delete js directory

--- js/state.js
+++
@@ -1,1 +0,0 @@
-let curCat = "all";
--- js/toast.js
+++
@@ -1,8 +0,0 @@
-let tT;
-function toast(msg) {
-  const t = document.getElementById("toast");
-  t.textContent = msg;
-  t.classList.add("on");
-  clearTimeout(tT);
-  tT = setTimeout(() => t.classList.remove("on"), 3000);
-}
